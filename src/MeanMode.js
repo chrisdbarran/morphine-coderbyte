fa12db8dcb05a65c6641e7e6fe33afce5cb038d7
Chris Barran <chris.d.barran@gmail.com>
2015-05-16 12:38:06 +0100

    Completed the MeanMode Challenge

---
+++ src/MeanMode.js
@@ -0,0 +1,33 @@
+var exports = module.exports = {};
+
+exports.MeanMode = function (arr) { 
+   var mean = this.findMean(arr);
+   var mode = this.findMode(arr);
+
+  return (mean == mode) ? 1 : 0;
+}
+
+exports.findMean = function (arr) {
+  var total = arr.reduce(function(a,b) {
+    return a + b;
+  });
+
+  return total / arr.length;
+}
+
+exports.findMode = function (arr) {
+    var cnts = {};
+
+    // Count the insidence of each number
+    arr.forEach(function (item) {
+        (cnts.hasOwnProperty(item)) ? cnts[item]++ : cnts[item] = 1;
+    });
+
+    // find the one that occurs most often.
+    var mode = Object.getOwnPropertyNames(cnts).reduce(function(p,v){
+        return (cnts[p] > cnts[v]) ? p : v;
+    });
+
+    // If the calculated mode only occurs once there is no mode so return -1
+    return (cnts[mode] == 1) ? -1 : mode;
+}
---
+++ src/MeanModeTest.js
@@ -0,0 +1,41 @@
+var test = require('./MeanMode.js')
+
+var assert = require("assert")
+
+describe('findMean(arr)', function() {
+    it('should return 3 when arr = [5, 3, 3, 3, 1] ', function() {
+        assert.equal(3, test.findMean([5, 3, 3, 3, 1] ));
+    });
+    it('should return 4 when arr = [4, 4, 4, 6, 2]', function() {
+        assert.equal(4, test.findMean([4, 4, 4, 6, 2]));
+    });
+    it('should return 2 when arr = [1,2,3]', function() {
+        assert.equal(2, test.findMean([1,2,3]));
+    });
+});
+
+describe('findMode(arr)', function() {
+    it('should return 3 when arr = [5,3,3,3,1]', function() {
+        assert.equal(3, test.findMode([5,3,3,3,1]));
+    });
+    it('should return 4 when arr = [4, 4, 4, 6, 2]', function() {
+        assert.equal(4, test.findMode([4, 4, 4, 6, 2]));
+    });
+    it('should return -1 when arr = [1,2,3]', function() {
+        assert.equal(-1, test.findMode([1,2,3]));
+    });
+});
+
+describe('MeanMode(arr)', function(){
+
+    it('should return 1 when arr =  [5, 3, 3, 3, 1] ', function(){
+      assert.equal(1,  test.MeanMode( [5, 3, 3, 3, 1] ));
+    });
+    it('should return 1 when arr = [4, 4, 4, 6, 2]', function() {
+        assert.equal(1, test.MeanMode([4, 4, 4, 6, 2]));
+    });
+    it('should return 0 when arr = [1,2,3]', function() {
+        assert.equal(0, test.MeanMode([1,2,3]));
+    });
+});
+
