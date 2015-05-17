cbe54b02c1ad7c9e03d2a890f81787bd785eca9c
Chris Barran <chris.d.barran@gmail.com>
2015-05-17 22:34:40 +0100

    Completed the NumberAddition Challenge

---
+++ src/NumberAddition.js
@@ -0,0 +1,16 @@
+var exports = module.exports = {};
+
+exports.NumberAddition = function (str) {
+    
+   // regexp to match all the numbers
+   var pattern = /\d+/g;
+
+   var res = str.match(pattern);
+
+   if(res == null) return 0;
+
+   // Then add them up. 
+   return res.reduce(function (p,v) {
+     return Number(p) + Number(v);
+   });
+}
