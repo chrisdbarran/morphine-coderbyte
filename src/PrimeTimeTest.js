f758e2a12aea3eee66e7825c12eb6f02f7f9d006
Chris Barran <chris.d.barran@gmail.com>
2015-05-21 16:25:21 +0100

    Completed the PrimeTime Challenge.

---
+++ src/PrimeTimeTest.js
@@ -0,0 +1,20 @@
+var test = require('./PrimeTime.js')
+var assert = require('assert')
+
+describe('PrimeTime(num)', function () {
+    it('Should return true for 2', function () {
+        assert.equal(true, test.PrimeTime(2))
+    })
+    it('Should return true for 3', function () {
+        assert.equal(true, test.PrimeTime(3))
+    })
+    it('Should return true for 13' , function () {
+        assert.equal(true, test.PrimeTime(13))
+    })
+    it('Should return false for 10', function () {
+        assert.equal(false, test.PrimeTime(10))
+    })
+    it('Should return false for 20000', function () {
+        assert.equal(false, test.PrimeTime(20000))
+    })
+})
