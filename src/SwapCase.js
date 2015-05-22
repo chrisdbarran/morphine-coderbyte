0eea38e584479235290f8202f9d5a35e1f944946
Chris Barran <chris.d.barran@gmail.com>
2015-05-22 09:23:56 +0100

    Original questions added as headers to implementations.

--- src/SwapCase.js
+++ src/SwapCase.js
@@ -1,3 +1,8 @@
+/*
+Have the function SwapCase(str) take the str parameter and swap the case of 
+each character. For example: if str is "Hello World" the output should be hELLO 
+wORLD. Let numbers and symbols stay the way they are. 
+*/
 var exports = module.exports = {};
 
 exports.SwapCase = function (str) {
