395b9320fab468594e50983fd355d44e9cf9a31e
Chris Barran <chris.d.barran@gmail.com>
2015-05-26 08:03:05 +0100

    ArithGeo tested for non geometric and non arithmetic sequence.

--- test/ArithGeoTest.js
+++ test/ArithGeoTest.js
@@ -8,4 +8,7 @@ describe('ArithGeo(arr)', function () {
     it('Should return "Geometric" for [2, 6, 18, 54]', function() {
         assert.equal('Geometric', test.ArithGeo([2, 6, 18, 54]));
     });
+    it('Should return "-1" for [2, 2, 1, 3, 5]', function () {
+        assert.equal('-1', test.ArithGeo([2, 2, 1, 3, 5]));
+    });
 });
